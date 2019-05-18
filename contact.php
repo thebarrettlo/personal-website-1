cd16cd00e70f85d58428593b6dfa6664bf7093fc
Barrett Lo <beebearlo@gmail.com>
2019-05-18 12:25:40 +0300

    Improved contact form spam score (still going to spam though), changed e-mail routing -- improved contact form overall.

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ if($_POST) {
     $messageSubject = "";
     $visitorMessage = "";
     $copyMessage = "";
-    $recipient = "thebarrettlo@gmail.com";
+    $recipient = "barrett@thebarrettlo.com";
     
     if(isset($_POST['visitorName'])) {
         $visitorName = filter_var($_POST['visitorName'], FILTER_SANITIZE_STRING);
@@ -23,28 +23,33 @@ if($_POST) {
 
     if(isset($_POST['visitorMessage'])) {
         $visitorMessage = htmlspecialchars($_POST['visitorMessage']);
-        $copyMessage = nl2br("Hi!\r\nThis message is to confirm your message sent to Barrett Lo (thebarrettlo@gmail.com) through
-        www.thebarrettlo.com. You'll find a copy of your message below. Please allow a couple days for a
-        reply. Thanks!\r\n\n##############################################\r\nSent on " . date("l, m/d/Y")
-        . " at " . date("h:i:sa") . ".\r\n\n$visitorMessage\r\n\n##############################################");
+        $copyMessage = "<html>" . nl2br("Hi!\r\n\nThis message is to confirm your message sent to Barrett Lo on www.thebarrettlo.com.
+        You'll find a copy of your message below. Please allow a couple days for a reply. Thanks!\r\n\n----------Begin Message----------\r\nSent on "
+        . date("l, m/d/Y") . " at " . date("h:i:sa") . ".\r\n\n$visitorMessage\r\n\n-----------End Message-----------") . "</html>";
     }
 
-    $headers = "MIME-Version: 1.0 " . "\r\n" . "Content-type: text/html; charset=utf-8 " . "\r\n" . 
-    "From: $visitorName <$contactEmail>\r\n";
+    $headers = "MIME-Version: 1.0\r\n" . "Content-type: text/html; charset=utf-8\r\n" .
+    "X-Priority: 4\r\n" . "Reply-To: $visitorName <$contactEmail>\r\n" . "From: $visitorName <$contactEmail>\r\n";
 
-    $headersAlt = "MIME-Version: 1.0" . "\r\n" . "Content-type: text/html; charset=utf-8" . "\r\n" .
-    "From: Barrett Lo <$recipient>\r\n";
+    $headersReceipt = "MIME-Version: 1.0\r\n" . "Content-type: text/html; charset=utf-8\r\n" .
+    "X-Priority: 5\r\n" . "Reply-To: Barrett Lo <$recipient>\r\n" . "From: Barrett Lo <$recipient>\r\n";
+
+    $envelopeFrom = "-f barrett@thebarrettlo.com";
 
     if(mail($recipient, $messageSubject, $visitorMessage, $headers) and
-    mail($contactEmail, "Successfully sent: $messageSubject", $copyMessage, $headersAlt)) {
+    mail($contactEmail, "Message sent: $messageSubject", $copyMessage, $headersReceipt, $envelopeFrom)) {
         echo "<script type='text/javascript'>
-             alert('Your message has been sent. You will receive a copy of your message and a reply soon!');
+             alert('Your message has been sent. You will receive a copy of your message and a reply soon! You may need to check your spam folder.');
              window.location.replace('https://www.thebarrettlo.com/contact.html');
              </script>";
         exit;
     }
     else {
-        echo "<p>Sorry, something went wrong! Please try back again later.</p>";
+        echo "<script type='text/javascript'>
+             alert('Sorry, something went wrong. Please try again later!');
+             window.location.replace('https://www.thebarrettlo.com/contact.html');
+             </script>";
+        exit;
     }
 }
 else {
