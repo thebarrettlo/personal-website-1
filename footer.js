d83db8d00d13c3b6c5c4f5924dca68e1be436adf
Barrett Lo <beebearlo@gmail.com>
2019-06-02 14:57:45 +0300

    v. 1.2.7: Rounded corners (3px) on white boxes, small adjustment to index.html, changed 1024px width on photo sets to two columns.

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 var p = document.createElement("p");
-var version = document.createTextNode("Version 1.2.6");
+var version = document.createTextNode("Version 1.2.7");
 br = document.createElement("br");
 var copyright = document.createTextNode("Barrett Lo \u00A9 2019");
 p.appendChild(version);
